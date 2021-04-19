cc649689f00f9629868a45554b69157573abaaa7
Adam Rutherford-Shaw <adam.rutherfordshaw@autotrader.co.uk>
2021-04-19 12:07:08 +0100

    add first test

---
+++ exercises/javascript/main/bowling/bowling.js
@@ -0,0 +1,11 @@
+class BowlingGame {
+  constructor() {}
+
+  bowl(rolls) {
+    return 0;
+  }
+}
+
+module.exports = {
+  BowlingGame,
+};
--- exercises/javascript/test/bowling/bowling.test.js
+++ exercises/javascript/test/bowling/bowling.test.js
@@ -1,5 +1,15 @@
-describe('Bowling tests', () => {
-  test('Rename me', () => {
+const { BowlingGame } = require("../../main/bowling/bowling.js");
 
+describe("Bowling tests", () => {
+  test("Gutter game", () => {
+    // Arrange
+    const bowlingGame = new BowlingGame();
+    const rolls = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+    // Act
+    const score = bowlingGame.bowl(rolls);
+
+    // Assert
+    expect(score).toBe(0);
   });
 });
