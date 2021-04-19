76247bb321a757bb1688a44adedb76e1649a549d
Adam Rutherford-Shaw <adam.rutherfordshaw@autotrader.co.uk>
2021-04-19 15:55:57 +0100

    add strike and refactor

--- exercises/javascript/main/bowling/bowling.js
+++ exercises/javascript/main/bowling/bowling.js
@@ -1,23 +1,23 @@
-class BowlingGame {
-  constructor() {}
+const bowl = (rolls) => {
+  let score = 0;
+  for (let roll = 0; roll < rolls.length; roll += 2) {
+    const firstScore = rolls[roll + 0];
+    const secondScore = rolls[roll + 1];
+    const thirdScore = rolls[roll + 2];
 
-  bowl(rolls) {
-    let score = 0;
-    for (let roll = 0; roll < rolls.length; roll += 2) {
-      const firstScore = rolls[roll + 0];
-      const secondScore = rolls[roll + 1];
+    score += firstScore + secondScore;
 
-      score += firstScore + secondScore;
-
-      if (firstScore + secondScore === 10) {
-        const thirdScore = rolls[roll + 2];
-        score += thirdScore;
-      }
+    if (firstScore + secondScore === 10) {
+      score += thirdScore;
     }
-    return score;
+    if (firstScore === 10) {
+      score += thirdScore;
+      roll--;
+    }
   }
-}
+  return score;
+};
 
 module.exports = {
-  BowlingGame,
+  bowl,
 };
--- exercises/javascript/test/bowling/bowling.test.js
+++ exercises/javascript/test/bowling/bowling.test.js
@@ -1,13 +1,12 @@
-const { BowlingGame } = require("../../main/bowling/bowling.js");
+const { bowl } = require("../../main/bowling/bowling.js");
 
 describe("Bowling tests", () => {
-  const bowlingGame = new BowlingGame();
   test("Gutter game", () => {
     // Arrange
     const rolls = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 
     // Act
-    const score = bowlingGame.bowl(rolls);
+    const score = bowl(rolls);
 
     // Assert
     expect(score).toBe(0);
@@ -16,7 +15,7 @@ describe("Bowling tests", () => {
   test("When we knock 1 pin per roll", () => {
     const rolls = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
 
-    const score = bowlingGame.bowl(rolls);
+    const score = bowl(rolls);
 
     expect(score).toBe(20);
   });
@@ -24,8 +23,16 @@ describe("Bowling tests", () => {
   test("Should have correct scoring for a spare in the first frame", () => {
     const rolls = [5, 5, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 
-    const score = bowlingGame.bowl(rolls);
+    const score = bowl(rolls);
 
     expect(score).toBe(12);
+  });
+
+  test("Should have correct scoring for a strike in the first frame", () => {
+    const rolls = [10, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+    const score = bowl(rolls);
+
+    expect(score).toBe(14);
   });
 });
