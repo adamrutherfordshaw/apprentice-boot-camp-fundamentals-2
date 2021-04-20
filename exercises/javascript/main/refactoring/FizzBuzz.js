54af144447628a31513672afee931e7e7ac127e4
Adam Rutherford-Shaw <adam.rutherfordshaw@autotrader.co.uk>
2021-04-20 14:58:30 +0100

    refactor again

--- exercises/javascript/main/refactoring/FizzBuzz.js
+++ exercises/javascript/main/refactoring/FizzBuzz.js
@@ -1,48 +1,51 @@
 const datatypeConverter = require("../util-datatype-converter");
 
-let currentNumber = 0;
-let threeCounter = 0;
-let fiveCounter = [0, 0, 0, 0, 0].length;
+let currentNumber = 1;
 
 function fizzBuzzGenerator() {
   let result = "";
   const maxNumber = 100;
-  for (; currentNumber < maxNumber; currentNumber++)
+  for (; currentNumber <= maxNumber; currentNumber++)
     result += fizzBuzzCalculator(currentNumber) + " ";
-  const trimmedResult = result.substring(0, result.length - 1);
+  const trimmedResult = result.trim();
   console.log(trimmedResult);
   return trimmedResult;
 }
 
 function buzz() {
-  fiveCounter = [0, 0, 0, 0, 0].length;
-  const hexBuzz = "42757a7a";
-  const buzzResult = String.fromCharCode.apply(
-    null,
-    datatypeConverter.parseHexString(hexBuzz) // "buzz"
-  );
-  return buzzResult;
+  return "Buzz";
 }
 
 function fizz() {
-  threeCounter = 0;
-  const hexFizz = "46697a7a";
-  const fizzResult = String.fromCharCode.apply(
-    null,
-    datatypeConverter.parseHexString(hexFizz) // "fizz"
-  );
-  return fizzResult;
+  return "Fizz";
 }
 
+// function isDivisible(currentNumber, divisbleNumber) {
+//   const isFizz = currentNumber % 3 === 0;
+//   const isBuzz = currentNumber % 5 === 0;
+//   let result = "";
+//   if (!(isFizz || isBuzz)) result += currentNumber;
+//   else {
+//     if (isFizz) result += fizz();
+//     if (isBuzz) result += buzz();
+//   }
+//   return result;
+// }
+
 function fizzBuzzCalculator(currentNumber) {
-  threeCounter++;
-  fiveCounter--;
-  const binaryThree = 0b11; // 0b11 == 3
-  const isFizz = threeCounter == binaryThree;
-  const isBuzz = fiveCounter == 0;
-  let result = isFizz || isBuzz ? "" : currentNumber + 1;
-  if (isFizz) result += fizz();
-  if (isBuzz) result += buzz();
+  let result = "";
+
+  const isFizz = currentNumber % 3 === 0;
+  const isBuzz = currentNumber % 5 === 0;
+  //let result = "";
+  if (!(isFizz || isBuzz)) result += currentNumber;
+  else {
+    if (isFizz) result += fizz();
+    if (isBuzz) result += buzz();
+  }
+
+  //result += isDivisible(currentNumber);
+
   return result;
 }
 
